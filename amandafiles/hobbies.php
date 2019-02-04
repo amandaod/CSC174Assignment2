9749b360d73cf665c0645714bf363f5ea6ceb678
amandaod <40402949+amandaod@users.noreply.github.com>
2019-02-04 10:41:44 -0500

    my files upload

---
+++ amandafiles/hobbies.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Hobbies | Amanda O'Donnell | Lab 1: Initial Personal Website</title>
+    <?php include 'inc/head.inc';?>
+</head>
+<body class="container">
+    <?php include 'inc/top.inc';?>
+    <main>
+        <h2>Hobbies</h2>
+        <p>In addition to my academic and curricular pursuits, I have a number of hobbies.  I really enjoy dancing, and to keep up with this interest I have completed a Dance cluster as well as been a member of Ballet Performance Group.  This semester, I hope to join Rochester Rangoli to learn a bit of Bollywood dance.  I also really enjoy making different types of crafts, although I’ve had less time to pursue this activity as a college student.  One of my favorite types of crafts is jewelry-making.  In fact, I got my ears pierced in high school to justify keeping the pairs of earrings I had made and really liked.  I also enjoy playing games, particularly hidden identity games.  Some of my recent favorites have been Avalon and Tempel des Schreckens.  I also enjoy playing my clarinet, ice skating, outdoor adventures, kayaking, and playing video games.  I like how simple excursions become adventures if you do them after midnight or in a snowstorm, and I went to Wegman’s during Snowmaggedon.</p>
+    </main>
+    <?php include 'inc/footnav.inc';?>
+
+
+<script src="https://code.jquery.com/jquery.js"></script>
+<script src="js/menu-highlighter.js"></script>
+</body>
+</html>
