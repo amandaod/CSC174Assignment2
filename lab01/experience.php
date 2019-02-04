9e99fbf4a560263182b60e646c8e21a22a986f9f
joehur1997 <35182470+joehur1997@users.noreply.github.com>
2019-02-04 10:45:49 -0500

    Add files via upload

---
+++ lab01/experience.php
@@ -0,0 +1,43 @@
+<?php include ('inc/html-top.inc'); ?>
+
+<body class="container">
+    <header>
+        <a href="index.php">
+            <h1>Joseph Hur</h1>
+            <img src="images/jhur.jpg" alt="Joseph Hur">
+        </a>
+    </header>
+
+    <aside>
+        <ul>
+            <li>Student @ University of Rochester</li>
+            <li>Computer Science/Anthropology Major</li>
+            <li>Aspiring Web/Software Developer</li>
+        </ul>
+    </aside>
+
+    <main>
+        <h2>Experience</h2>
+        <p>
+            For the summer of 2018, I worked at a summer educational program as their C.S instructor. My job involved
+            teaching middle schoolers the basics of computer science. I taught the basics principles of computer
+            science, such as the purpose of writing algorithms, as well as introductory programming skills in Python.
+            By the end of the summer, my students were able to write loops and conditionals in Python, as well as
+            knowing how to utilize arrays and other data types. I also maintained the technology within the
+            institution. It was a great opportunity, as besides teaching being something that I enjoy, it gave me to
+            opportunity to keep engaged with computer science over the summer.
+        </p>
+    </main>
+
+    <footer>
+        <h4>
+            CSC 174: Advanced Front-End Web Design and Development
+        </h4>
+    </footer>
+
+    <?php include ('inc/nav.inc'); ?>
+    <?php include ('inc/scripts.inc'); ?>
+
+</body>
+
+</html>
---
+++ lab01/index.php
@@ -0,0 +1,43 @@
+<?php include ('inc/html-top.inc'); ?>
+
+<body class="container">
+    <header>
+        <a href="index.php">
+            <h1>Joseph Hur</h1>
+            <img src="images/jhur.jpg" alt="Joseph Hur">
+        </a>
+    </header>
+
+    <aside>
+        <ul>
+            <li>Student @ University of Rochester</li>
+            <li>Computer Science/Anthropology Major</li>
+            <li>Aspiring Web/Software Developer</li>
+        </ul>
+    </aside>
+
+    <main>
+        <p>
+            I am currently a 4th year student at the University of Rochester studying both Computer Science and
+            Anthropology. I was born in Fort Worth, Texas, but have lived in Hawaii (where one of my sisters was born)
+            and New Jersey (where my other sister was born). I grew up mostly in Bergen County, NJ, especially within
+            the Korean-American enclave of Palisades Park. Growing up I was always fascinated by computers and how they
+            worked. I remember milestones like the process of installing my first graphics card, and learning how to
+            mod video games.This semester, I am taking CSC174 and CSC 261. I hope to learn more advanced concepts of
+            front-end development from 174, and gain a more intricate understanding of relational databases through
+            261.
+        </p>
+    </main>
+
+    <footer>
+        <h4>
+            CSC 174: Advanced Front-End Web Design and Development
+        </h4>
+    </footer>
+
+    <?php include ('inc/nav.inc'); ?>
+    <?php include ('inc/scripts.inc'); ?>
+
+</body>
+
+</html>
