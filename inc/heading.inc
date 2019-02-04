4a0dfd4c9ca66affa14ce634f584db02b1690eec
Amanda <amanda.k.odonnell@gmail.com>
2019-02-04 11:39:17 -0500

    changes to file structure

---
+++ inc/heading.inc
@@ -0,0 +1,17 @@
+<?php
+echo '<header>
+<a href="index.php">
+    <h1>
+        Won Kuk Lee
+    </h1>
+    <img src="images/wonkuk-headshot.jpg" alt="headshot">
+</a>
+</header>
+<div class="roles">
+<ul>
+    <li> Software Engineer </li>
+    <li> Senior Student at UR</li>
+    <li> CS Major</li>
+</ul>
+</div>';
+?>
---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Team Boston | Lab 2: "Flash" Website</title>
+    <?php include 'inc/head.inc';?>
+</head>
+
+<body class="container">
+    <main>
+        <header>
+            <a href="amanda.php">
+                <h1>Amanda O'Donnell</h1>
+                <img src="images/amanda.jpg" alt="Amanda O'Donnell">
+            </a>
+        </header>
+        <p>I am a student here at University of Rochester finishing up my major in Brain & Cognitive Sciences as well
+            as minors in Computer Science and Psychology. I knew coming into college that I wanted to major in Brain &
+            Cognitive Sciences because it seemed like a combination of biology and psychology—two subjects I had loved
+            in high school. I had originally planned on double majoring in psychology along with my Brain & Cognitive
+            Sciences major, but when I took The Science of Programming (CSC 171) as part of my Brain & Cognitive
+            Sciences program I loved it and decided to stick with it. I’m very glad I took that class early on in my
+            college career so that I was able to add a Computer Science minor and explore that newfound interest.</p>
+        <header>
+            <a href="jacob.php">
+                <h1>Jacob Lee</h1>
+                <img src="images/wonkuk-headshot.jpg" alt="Joseph Hur">
+            </a>
+        </header>
+        <p>
+            My name is Jacob Lee and I am currently a senior at the University of Rochester. I’m majoring in Computer
+            Science and plan to graduate this May with a Bachelor of Science. I have a passion for transforming
+            innovative ideas to seamless and elegant web applications. When I was a high schooler, I held a plethora of
+            different jobs such as yogurt, smoothie, and bubble tea making, chicken serving, and SAT teaching. Two
+            summers ago I worked at ClassLink as a Junior Developer Intern and would say that it wasn’t a pleasant
+            experience as I did not have much mentorship. However, I still learned a lot by myself through
+            stackoverflow and learning from my mistakes. The experience was so awful that I thought I would hate the
+            software engineering industry.
+        </p>
+        <header>
+            <a href="joseph.php">
+                <h1>Joseph Hur</h1>
+                <img src="images/jhur.jpg" alt="Joseph Hur">
+            </a>
+        </header>
+        <p>
+            I am currently a 4th year student at the University of Rochester studying both Computer Science and
+            Anthropology. I was born in Fort Worth, Texas, but have lived in Hawaii (where one of my sisters was born)
+            and New Jersey (where my other sister was born). I grew up mostly in Bergen County, NJ, especially within
+            the Korean-American enclave of Palisades Park. Growing up I was always fascinated by computers and how they
+            worked. I remember milestones like the process of installing my first graphics card, and learning how to
+            mod video games.This semester, I am taking CSC174 and CSC 261. I hope to learn more advanced concepts of
+            front-end development from 174, and gain a more intricate understanding of relational databases through
+            261.
+        </p>
+    </main>
+    <?php include ('inc/nav.inc'); ?>
+</body>
+
+</html>
